32018cf9e5e67390848946cf200d9a5aee77ca88
Akshay771 <96593557+Akshay771@users.noreply.github.com>
2024-04-11 13:13:06 +0530

    Update views.py

--- app/master/views.py
+++ app/master/views.py
@@ -53,7 +53,7 @@ class FlaskHealthCheck(Resource):
 class TestEndPoint(Resource):
 
     def get(self):
-        return make_response(jsonify({"Modified Version": "v3"}), 200)
+        return make_response(jsonify({"Modified Version": "v4"}), 200)
 
 
 # class CeleryHealthCheck(Resource):
